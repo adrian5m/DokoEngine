166a2165dffe98b4900ea7abb65cc1eb726a932d
Adrian <adrian.michelmann@rwth-aachen.de>
2023-05-21 13:16:15 +0200

    added player class

---
+++ src/players.py
@@ -0,0 +1,39 @@
+from cards import Card
+
+
+class Player(object):
+    def __init__(self, name: str):
+        """
+        Args:
+            name (str): Name of the player
+        """
+        self.name = name
+        self.hand = []
+        self.tricks = []
+        self.points = 0
+        self.team = None
+
+    def __repr__(self):
+        return self.name
+
+    def play_card(self, card: str) -> Card:
+        """Play a card
+
+        Args:
+            card (str): Name of the card to play
+
+        Returns:
+            Card
+        """
+        self.__sanity_check(card)
+        played_card = next((x for x in self.hand if x.name == card), None)
+        self.hand.remove(played_card)
+        return played_card
+
+    def __sanity_check(self, played_card: str):
+        """Checks if the card is in the player's hand
+
+        Args:
+            played_card (str): Name of the card to play
+        """
+        assert played_card in [card.name for card in self.hand], "The selected card is not in your hand"
