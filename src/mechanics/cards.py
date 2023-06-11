169aad8ed4728240912c0033b4431b31d85172be
Adrian <adrian.michelmann@rwth-aachen.de>
2023-06-11 13:47:49 +0200

    added Suit and Face classes

--- src/mechanics/cards.py
+++ src/mechanics/cards.py
@@ -1,19 +1,33 @@
 import random
+from enum import Enum
 
+class Suit(Enum):
+    CLUBS = 'Clubs'
+    SPADES = 'Spades'
+    HEARTS = 'Hearts'
+    DIAMONDS = 'Diamonds'
+
+class Face(Enum):
+    NINE = 0
+    TEN = 10
+    JACK = 2
+    QUEEN = 3
+    KING = 4
+    ACE = 11
 
 class Card(object):
     """French-suited playing card with certain suit, face and value as reward"""
-    def __init__(self, suit: str, face: str, value: int):
+    def __init__(self, suit: Suit, face: Face):
         """
         Args:
             suit (str): Suit of the card
             face (str): Face of the card
             value (int): The card's reward at the end of a round
         """
-        self.suit = suit
-        self.face = face
-        self.value = value
-        self.name = f'{suit[0]}{face if len(face) < 3 else face[0]}'
+        self.suit = suit.name
+        self.face = face.name
+        self.value = face.value
+        self.name = f'{self.suit[0]}{self.face if len(self.face) < 3 else self.face[0]}'
         self.is_trump = False
         self.trump_value = 0
         
@@ -24,39 +38,39 @@ class Card(object):
 
 class Deck(object):
     """Doppelkopf card deck"""
-    SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
-    VALUES = {'9': 0, '10': 10, 'Jack': 2, 'Queen': 3, 'King': 4, 'Ace': 11}
-    TRUMPS = {'Hearts 10': 13,
-              'Clubs Queen': 12,
-              'Spades Queen': 11,
-              'Hearts Queen': 10,
-              'Diamonds Queen': 9,
-              'Clubs Jack': 8,
-              'Spades Jack': 7,
-              'Hearts Jack': 6,
-              'Diamonds Jack': 5,
-              'Diamonds Ace': 4,
-              'Diamonds 10': 3,
-              'Diamonds King': 2,
-              'Diamonds 9': 1
+    TRUMPS = {'TEN of HEARTS': 13,
+              'QUEEN of CLUBS': 12,
+              'QUEEN of SPADES': 11,
+              'QUEEN of HEARTS': 10,
+              'QUEEN of DIAMONDS': 9,
+              'JACK of CLUBS': 8,
+              'JACK of SPADES': 7,
+              'JACK of HEARTS': 6,
+              'JACK of DIAMONDS': 5,
+              'ACE of DIAMONDS': 4,
+              'TEN of DIAMONDS': 3,
+              'KING of DIAMONDS': 2,
+              'NINE of DIAMONDS': 1
               }
 
-    def __init__(self, card: Card, exclude_nine: bool = False):
+    def __init__(self, card: Card, exclude_nine: bool = False, suits = Suit, faces = Face):
         """
         Args:
             card (Card): Playing card
             exclude_nine (bool, optional): Flag for excluding all the "nine" cards
         """
+        self.suits = suits
+        self.faces = faces
         self.stack = []
-        for suit in self.SUITS:
-            for face, reward in self.VALUES.items():
-                if exclude_nine and face == '9':
+        for suit in self.suits:
+            for face in self.faces:
+                if exclude_nine and face.name == 'NINE':
                     continue
 
-                current_card = card(suit, face, reward)
-                if f'{suit} {face}' in self.TRUMPS:
+                current_card = card(suit, face)
+                if str(current_card) in self.TRUMPS:
                     current_card.is_trump = True
-                    current_card.trump_value = self.TRUMPS[f'{suit} {face}']
+                    current_card.trump_value = self.TRUMPS[str(current_card)]
 
                 self.stack.append(current_card)
                 self.stack.append(current_card)
