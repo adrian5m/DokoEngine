7a7a5253d829a5eaafc8ca3d3206dc2b6ff8c9ac
Adrian <adrian.michelmann@rwth-aachen.de>
2023-06-12 21:13:57 +0200

    Extended game state functionality

--- src/mechanics/game.py
+++ src/mechanics/game.py
@@ -12,16 +12,75 @@ class Round(object):
         """
         self.game_state = {
             'players': players,
+            'teams': ['', '', '', ''],
             'current_player': players[0],
             're_calls': [],
             'contra_calls': [],
             'trick': [],
             'last_trick': [],
-            '_played_tricks': [],    
+            '_played_tricks': [], 
+            'wedding': False,
+            'solo': False,
+            'solo_type': None   
         }
         self.round_finished = False
         self.play()
+    
+    def parse_game_state(self) -> dict:
+        """Parses the public game state for the players"""
+        public_state = {key: value for key, value in self.game_state.items() if not key.startswith('_')}
+        for player in public_state['players']:
+            player.team = None
+        # todo: maybe hide the round points of each player
+        return public_state
+    
+    def update_game_state(self, idx: int, trick: list[Card] = None, card: Card = None, call = None) -> None:
+        """Updates the game state based on a played card, trick or made call
+        Args:
+            idx (int): index is dependent on the other arguments. If trick, idx means the idx of the starting player, if card or call is done then idx indicates the current players idx
+            trick (list[Card]): Trick containing 4 cards. Defaults to None.
+            card (Card, optional): Played card. Defaults to None.
+            call (_type_, optional): Made Call. Defaults to None.
+
+        Raises:
+            ValueError: When trick and card are parsed simultaneously
+        """
+        if trick is not None and card is not None:
+            raise ValueError("the game state should not be updated for a card and a trick simultaneously")
         
+        elif card is not None:
+            self.game_state['trick'].append(card)
+            if str(card) == 'QUEEN of CLUBS' and not self.game_state['solo']:
+                self.game_state['teams'][idx] = 'Re'
+                # check if the other team mate is already known and update other player's team accordingly
+                if self.game_state['teams'].count('Re') == 2:
+                    self.game_state['teams'] = list(map(lambda x: 'Contra' if x == '' else x, self.game_state['teams']))
+                
+                # special case for a silent wedding 
+                elif self.game_state.get('_first_re_card', False) and self.game_state.get('_first_re_card', False) == self.game_state['current_player'].name:
+                    self.game_state['teams'] = list(map(lambda x: 'Contra' if x == '' else x, self.game_state['teams']))
+                    self.game_state.update({'solo': True,
+                                            'solo_type': 'Trump Solo'})
+                
+                self.game_state.update({'_first_re_card': self.game_state['current_player'].name})
+        
+        elif trick is not None:
+            # evaluate trick. make winner start next trick
+            # update played tricks, last trick, current player
+            # todo implement evaluation of card based bonus points
+            self.game_state['_played_tricks'].append(self.game_state['trick'])
+            winner_idx, round_points = self.eval_trick(trick=self.game_state['trick'], start_idx=idx)
+            self.game_state['players'][winner_idx].round_points += round_points
+            self.game_state.update({'last_trick': (self.game_state['players'][winner_idx].name, self.game_state['trick'])})
+            self.game_state['trick'].clear()
+            self.game_state['current_player'] = self.game_state['players'][winner_idx]
+            
+        if call is not None:
+            self.game_state['re_calls'].append(call) if self.game_state['players'][idx].team == 'Re' else self.game_state['re_calls']
+            self.game_state['teams'][idx] = self.game_state['players'][idx].team
+            if self.game_state['teams'].count(self.game_state['players'][idx].team) == 2:
+                    self.game_state['teams'] = [player.team for player in self.game_state['players']]
+                
     def get_current_player(self) -> Player:
         """Returns the current player"""
         return self.game_state['current_player']
@@ -31,7 +90,7 @@ class Round(object):
         for player in self.game_state['players']:
             player.team = 'Re' if 'CQ' in [card.name for card in player.hand] else 'Contra'
 
-    def eval_trick(self, trick: list[Card], start_idx: int) -> int:
+    def eval_trick(self, trick: list[Card], start_idx: int) -> tuple[int, int]:
         """Evaluate the trick and give the rewarding points to the winner
 
         Args:
@@ -39,7 +98,7 @@ class Round(object):
             start_idx (int): Index of player who started the trick
 
         Returns:
-            int: Resulting index of the player who has won
+            tuple[int, int]: Tuple of the winner's index and the round points  
         """
         trick_points = sum([card.value for card in trick])
         trump_values = [card.trump_value for card in trick]
@@ -62,9 +121,8 @@ class Round(object):
         if result_idx > 3: 
             result_idx -= 4
 
-        self.game_state['players'][result_idx].round_points += trick_points
-        return result_idx
-        
+        return result_idx, trick_points
+    
     def play(self):
         """Executes the round"""
         self.assign_teams()
@@ -75,7 +133,7 @@ class Round(object):
             card_to_play = cards_on_hand[int(input(f"{player_in_action.name}: Play one of the following cards {cards_on_hand}:"))]
             card = player_in_action.play_card(card_to_play.name)
             suit_of_trick = 'Trump' if card.is_trump else card.suit
-            self.game_state['trick'].append(card)
+            self.update_game_state(idx=starting_player, card=card, call=None)
             idx = starting_player + 1
             while len(self.game_state['trick']) < 4:
                 if idx > 3:
@@ -96,15 +154,11 @@ class Round(object):
                     print('Your card did not match the criteria for following the trick. Please play another card.')
                     player_in_action.hand.append(card)
                 
-                self.game_state['trick'].append(card)
+                # todo update game state when a call is made
+                self.update_game_state(idx=idx, card=card, call=None)
                 idx += 1
             
-            # evaluate trick and make the winner to the person who will start the next trick
-            self.game_state['_played_tricks'].append(self.game_state['trick'])
-            winner_idx = self.eval_trick(trick=self.game_state['trick'], start_idx=starting_player)
-            self.game_state.update({'last_trick': (self.game_state['players'][winner_idx], self.game_state['trick'])})
-            self.game_state['trick'].clear()
-            self.game_state['current_player'] = self.game_state['players'][winner_idx]
+            self.update_game_state(idx=starting_player, trick=self.game_state['trick'])
             player_in_action = self.get_current_player()
             
             if not len(player_in_action.hand):
